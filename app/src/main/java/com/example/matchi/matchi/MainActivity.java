1182c46deddb6014b8e62045e5e03a9f0f8ba65d
theofficialwils <johnbwilson@outlook.com>
2016-05-10 15:20:44 +0100

    Facebook login
    Added Facebook login stuff

--- app/src/main/java/com/example/matchi/matchi/MainActivity.java
+++ app/src/main/java/com/example/matchi/matchi/MainActivity.java
@@ -1,8 +1,51 @@
 package com.example.matchi.matchi;
 
-import android.app.Activity;
-// login stuff happens here!
-public class MainActivity extends Activity
-{
+private TextView info;
+private LoginButton loginButton;
 
+public class MainActivity extends Activity {
+    private CallbackManager callbackManager;
+    private LoginButton loginButton;
+    private TextView btnLogin;
+    private ProgressDialog progressDialog;
+    User user;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+
+        callbackManager = CallbackManager.Factory.create();
+        setContentView(R.layout.login);
+        info = (TextView) findViewById(R.id.info);
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+    }
+
+    loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+        @Override
+        public void onSuccess(LoginResult loginResult) {
+            info.setText(
+                    "User ID: "
+                            + loginResult.getAccessToken().getUserId()
+                            + "\n" +
+                            "Auth Token: "
+                            + loginResult.getAccessToken().getToken()
+            );
+        }
+
+        @Override
+        public void onCancel() {
+            info.setText("Login attempt canceled.");
+        }
+
+        @Override
+        public void onError(FacebookException e) {
+            info.setText("Login attempt failed.");
+        }
+    });
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
 }
